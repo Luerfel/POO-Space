f0c8c4ae8d5c6d4524ef4a912fb5755b5429c14a
Luerfel <mluerfel@gmail.com>
2023-10-20 13:38:01 -0300

    s

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
-# Example file showing a circle moving on screen
 import pygame
+
+class Jogador:
+  def __init__(self):
+    self.posicao = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+  def mover(self, dt):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+      self.posicao.y -= 300 * dt
+    if keys[pygame.K_s]:  
+      self.posicao.y += 300 * dt
+    if keys[pygame.K_a]:
+      self.posicao.x -= 300 * dt      
+    if keys[pygame.K_d]:
+      self.posicao.x += 300 * dt
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+jogador = Jogador() 
 
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+  # event handling  
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+  # desenhar tela
+  screen.fill("dark blue")
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+  dt = clock.tick(60) / 1000
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+  jogador.mover(dt)
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+  pygame.draw.circle(screen, "red", jogador.posicao, 40)
+
+  pygame.display.flip()
 
 pygame.quit()

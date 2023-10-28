e4133a2a3b0b64eb76ac912f9daefc83223cd9c5
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2023-10-28 13:14:02 -0300

    added lost, player lose lives, and a claen code

--- background.py
+++ background.py
@@ -1,27 +1,50 @@
 import pygame
 import os
 
+from enemy import Enemy
+from player import Player
+
 class Background:
-    def __init__(self, img):
+    def __init__(self, w, h, img, surface):
         self.image = img
         self.rect = self.image.get_rect()
+
+        self.__WIN = surface
         
         self.rect_x = 0
         self.rect_y = 0
+        
+        self.__WIDTH = w
+        self.__HEIGHT = h
 
         
-    def draw(self, surface, lives, lvl):
-        # Desenhar apenas uma região da imagem
-        surface.blit(self.image, (self.rect_x, self.rect_y),
-        area=(self.rect_x, self.rect_y, 1280, 720))
+    def draw(self, player):
+        # draw the background
+        self.__WIN.blit(self.image, (self.rect_x, self.rect_y),
+        area=(self.rect_x, self.rect_y, self.__WIDTH, self.__HEIGHT))
 
-        font = pygame.font.SysFont('Arial', 30)   
+        font = pygame.font.SysFont('Arial', 30) # create a font
    
-        lives_text = font.render(f'Lives: {lives}', True, (255,255,255))
-        level_text = font.render(f'Level: {lvl}', True, (255,255,255))
+        lives_text = font.render(f'Lives: {player.get_lives()}', True, (255,255,255))
+        level_text = font.render(f'Level: {player.get_lvl()}', True, (255,255,255))
 
-        surface.blit(lives_text, (10,10)) 
-        surface.blit(level_text, (10,40))
+        self.__WIN.blit(lives_text, (10,10)) 
+        self.__WIN.blit(level_text, (10,40))
+
+        # draw the enemies on the screen
+        for enemy in Enemy.wave: 
+            enemy.draw(self.__WIN)
+    
+    def lost(self):
+        # draw the background
+        self.__WIN.blit(self.image, (self.rect_x, self.rect_y),
+        area=(self.rect_x, self.rect_y, self.__WIDTH, self.__HEIGHT))
+
+        font = pygame.font.SysFont('Arial', 60) # create a font
+
+        # draw the text on the screen
+        lost_text = font.render(f'You lost!', True, (255,255,255))
+        self.__WIN.blit(lost_text, (self.__WIDTH/2 - lost_text.get_width()/2, self.__HEIGHT/2 - lost_text.get_height()/2))
 
     
     def update(self):
--- main.py
+++ main.py
@@ -9,32 +9,34 @@ from enemy import Enemy
 
 pygame.init()
 
+# variables and constants that'll be used to run the game window ----------------------------------------------------------------------
 WIDTH, HEIGHT = 1280, 720  
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 
 pygame.display.set_caption("Space Invaders")
 
+background = Background(WIDTH, HEIGHT, pygame.image.load(os.path.join("assets", "background.jpg")), WIN)
+
+
 def main_game():
-
-    background = Background(pygame.image.load(os.path.join("assets", "background.jpg")))
-
+    # variables and constants that'll be used in the game ----------------------------------------------------------------------------
     run = True
     FPS = 60
     clock = pygame.time.Clock()
-    player = Player(WIDTH/2, HEIGHT/2, 5, pygame.image.load(os.path.join("assets", "main_ship.png")))  # create the player
 
-    while run :
-        clock.tick(FPS)
-        
-        # check of the game is closed
+    # create the player
+    PLAYER_SHIP = pygame.transform.scale(pygame.image.load(os.path.join("assets", "main_ship.png")), (70,70))
+    player = Player(WIDTH/2 - (PLAYER_SHIP.get_width()/2), HEIGHT - 100, 5, PLAYER_SHIP)  
+
+    # functions that'll be used in the game ------------------------------------------------------------------------------------------
+    def isClosed(): # check if the game is closed
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-        
-        if len(Enemy.wave) == 0:    # if there are no enemies (https://miro.medium.com/v2/resize:fit:958/1*P9xAw_nF6ohqlKMFpgcoxA.jpeg)
-            
-            # create the enemies
+                return True
+        return False
+
+    def next_lvl(): # go to the next level
+        # create the enemies
             for i in range(Enemy.wave_length):
                 enemy = Enemy(random.randint(50, WIDTH - 50), random.randint(-1500, -100), random.choice(["red", "green", "blue"]), random.choice([100, 150, 200]))
                 Enemy.wave.append(enemy)
@@ -42,14 +44,29 @@ def main_game():
             player.increment_lvl()  # go to the next level
             Enemy.wave_length += 5  # increase the number of enemies
             Enemy.speed_increment += 0.2 # increase the speed of the enemies
-            
-        background.draw(WIN, player.get_lives(), player.get_lvl()) # draw the background
+
+    # main game loop -----------------------------------------------------------------------------------------------------------------
+    while run :
+        clock.tick(FPS)
+        
+        if isClosed():
+            run = False
+        
+        if len(Enemy.wave) == 0: # if there are no enemies (https://miro.medium.com/v2/resize:fit:958/1*P9xAw_nF6ohqlKMFpgcoxA.jpeg)
+            next_lvl()
+
+        if player.get_lives() <= 0: # if the player lost
+            background.lost()
+            pygame.display.update()
+            time.sleep(3)
+            run = False
+
+        background.draw(player) # draw the background
 
         player.move_ship(pygame.key.get_pressed()) # move the ship
         player.draw(WIN, pygame.mouse.get_pos()) # draw the ship
 
         for enemy in Enemy.wave: # go through the enemies
-            enemy.draw(WIN)
             enemy.move()
             if enemy.y + enemy.get_height() > HEIGHT:
                 player.decrement_lives()
